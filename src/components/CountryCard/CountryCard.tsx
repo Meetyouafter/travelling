d6bbd0b3439a31b7d864f12fa2efb6eba2142f26
Meetyouafter <anton_levus@mail.ru>
2023-04-22 21:49:58 +0530

    added some cities

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -1,19 +1,22 @@
 import React, { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 import {
-  Typography, Card, CardMedia, CardContent, CardActions, Button,
+  Typography, Card, CardMedia, CardContent, CardActions, Button, Grid, Box,
 } from '@mui/material';
 import styles from './countryCard.module.scss';
 
 interface CountryProps {
   country: string;
+  capital: string;
   images: string[];
   description: string;
 }
 
 type BinaryStateForImage = 0 | 1;
 
-const CountryCard: FC<CountryProps> = ({ country, images, description }) => {
+const CountryCard: FC<CountryProps> = ({
+  country, images, description, capital,
+}) => {
   const [activeImageIndex, setActiveImageIndex] = useState<BinaryStateForImage>(0);
   const [data, setData] = useState('');
 
@@ -22,33 +25,46 @@ const CountryCard: FC<CountryProps> = ({ country, images, description }) => {
       .then((response) => console.log(response));
   }, []);
 
-  console.log(data)
+  console.log(data);
 
   return (
-    <Card
-      onMouseEnter={() => setActiveImageIndex(1)}
-      onMouseLeave={() => setActiveImageIndex(0)}
-      className={styles.card}
-    >
-      <CardMedia
-        sx={{ height: 140 }}
-        image={images[activeImageIndex]}
-        className={styles.card__image}
-        title={country}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {country}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Grid item className={styles.wrapper} xs={12}>
+      <Card
+        onMouseEnter={() => setActiveImageIndex(1)}
+        onMouseLeave={() => setActiveImageIndex(0)}
+        className={styles.card}
+        sx={{ backgroundColor: '#FDB750' }}
+      >
+        <CardMedia
+          image={images[activeImageIndex]}
+          className={styles.card__image}
+          title={country}
+        />
+        <CardContent className={styles.card__content}>
+          <Box>
+            <Typography gutterBottom variant="h5">
+              {country}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+          </Box>
+          <Box className={styles.card__content_info}>
+            <Typography variant="body2">
+              Capital:
+              {' '}
+              {capital}
+            </Typography>
+            <Typography variant="body2">
+              Weather:
+            </Typography>
+          </Box>
+        </CardContent>
+        <CardActions className={styles.card__footer}>
+          <Button size="small" variant="contained">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -8,6 +8,11 @@ declare module '*.jpg' {
   export default content;
 }
 
+declare module '*.jpeg' {
+  const content: any;
+  export default content;
+}
+
 declare module '*.scss' {
   const styles: { [className: string]: string };
   export default styles;
